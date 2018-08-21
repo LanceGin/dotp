22abb522cce18817dc6a5950b8bbceb12e1f9e38
lancegin <gin.lance.inside@hotmail.com>
2018-08-21 19:42:45 +0800

    upd: pubsec info

--- test/dotp_test.dart
+++ test/dotp_test.dart
@@ -1,13 +1,12 @@
-import 'package:test/test.dart';
-
 import 'package:dotp/dotp.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = new Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  TOTP a = TOTP("J22U6B3WIWRRBTAV");
+  print(a.now());
+
+  List<int> _result = [];
+  _result.addAll([0, 2, 3, 4, 0, 0, 0, 0]);
+  _result = _result.sublist(0, 8);
+  _result = _result.reversed.toList();
+  print(_result);
 }
