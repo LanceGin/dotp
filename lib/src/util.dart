6e3654850fcfeb3d98658ab2c78c7e6f1a990410
lancegin <gin.lance.inside@hotmail.com>
2018-08-21 14:21:17 +0800

    add: utils function timeFormat && intToByteList

---
+++ lib/src/util.dart
@@ -0,0 +1,48 @@
+///
+/// @module   : Util module to process the datas.
+/// @author   : Gin (gin.lance.inside@hotmail.com)
+///
+
+abstract class Util {
+  /// 
+  /// format the time string to int
+  /// 
+  /// @param {time}
+  /// @type {Date}
+  /// @desc the time need to be format
+  /// 
+  /// @param {interval}
+  /// @type {Int}
+  /// @desc interval means the one-time password's life,
+  /// default to be 30.
+  /// 
+  /// @return {Int}
+  /// 
+  static int timeFormat(DateTime time, int interval) {
+    final _timeStr = time.millisecondsSinceEpoch.toString();
+    final _formatTime = _timeStr.substring(0, _timeStr.length - 3);
+
+    return int.parse(_formatTime) ~/ interval;
+  }
+
+  /// 
+  /// transfer the int type to List type
+  /// 
+  /// @param {input}
+  /// @type {int}
+  /// @desc input param, maybe counter or time
+  /// 
+  /// @return {List}
+  /// 
+  static List intToBytelist(int input, [int padding = 8]) {
+    var _result = [];
+    var _input = input;
+    while (_input != 0) {
+      _result.add(String.fromCharCode(_input & 0xff));
+      _input >>= 8;
+    }
+    _result.addAll([0, 0, 0, 0, 0, 0, 0, 0]);
+    _result = _result.sublist(0, 7);
+    return _result.reversed;
+  }
+}
