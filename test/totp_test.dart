fb0cd072dcb4da7535ba2d3e72a66504b73e16b7
Bruno Miguens <brunomiguens@icloud.com>
2019-07-17 10:33:51 +0100

    Refactor dart_otp_test and split it into seperate files

--- test/totp_test.dart
+++ test/totp_test.dart
@@ -4,7 +4,6 @@ import 'package:test/test.dart';
 void main() {
 
     var totp = TOTP(secret: "J22U6B3WIWRRBTAV");
-    var hotp = HOTP(secret: "J22U6B3WIWRRBTAV");
 
   test('[TOTP] Should check the token with default digits and interval', () {
 
@@ -40,37 +39,6 @@ void main() {
 
     expect(totp.verify(otp: otpValue), false);
     expect(totp.verify(otp: otpValue, time: time), true);
-
-  });
-
-  test('[HOTP] Should check the token with default digits', () {
-
-    expect(hotp.digits, 6);
-    expect(hotp.secret, "J22U6B3WIWRRBTAV");
-
-  });
-
-  test('[HOTP] Should check the token with custom digits', () {
-
-    var token = HOTP(secret: "J22U6B3WIWRRBTAV", digits: 8);
-
-    expect(token.digits, 8);
-    expect(token.secret, "J22U6B3WIWRRBTAV");
-
-  });
-
-  test('[HOTP] Should generate and verify using a specific counter', () {
-
-    var otpValue = hotp.at(counter: 0);
-    expect(hotp.verify(otp: otpValue, counter: 0), true);
-
-  });
-
-  test('[HOTP] Should generate, verify using a specific counter and comare with a superior counter', () {
-
-    var otpValue = hotp.at(counter: 10);
-    expect(hotp.verify(otp: otpValue, counter: 10), true);
-    expect(hotp.verify(otp: otpValue, counter: 20), false);
 
   });
 
