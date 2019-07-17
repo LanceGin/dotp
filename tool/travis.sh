c9486d5c8856cc6df1c2153b2424bd602c78ed59
Bruno Miguens <brunomiguens@icloud.com>
2019-07-17 10:39:29 +0100

    Fix travis tool relative path

--- tool/travis.sh
+++ tool/travis.sh
@@ -22,21 +22,21 @@ for PKG in ${PKGS}; do
         echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
         case ${TASK} in
         dartanalyzer)
-        echo 'dartanalyzer --fatal-warnings .'
-        dartanalyzer --fatal-warnings . || EXIT_CODE=$?
+            echo 'dartanalyzer --fatal-warnings .'
+            dartanalyzer --fatal-warnings . || EXIT_CODE=$?
         ;;
         dartfmt)
             echo 'dartfmt -n --set-exit-if-changed .'
             dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
-            ;;
+        ;;
         test)
             echo 'pub run test --preset travis'
             pub run test --preset travis || EXIT_CODE=$?
-            ;;
+        ;;
         *)
             echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
             EXIT_CODE=1
-            ;;
+        ;;
         esac
     done
 
